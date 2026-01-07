4f5dd859346656a13fac46865f17e7d29fa99356
AvrahamTz <99908280+AvrahamTz@users.noreply.github.com>
2026-01-07 16:57:21 +0200

    Fix registration condition for username validation

--- services/usersS.js
+++ services/usersS.js
@@ -2,7 +2,7 @@ import {Users} from "../models/usersM.js"
 import { findByUsername , add} from "../repository/usersRe.js"
 
 export function register(username, password){
-    if(username !== false && findByUsername(username) !== false){
+    if(username !== "" && findByUsername(username) === false){
       const user =new Users(username,password)
       add(user)
     }
